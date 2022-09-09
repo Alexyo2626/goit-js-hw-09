f86e24e5db7fdba0c0ffec53380437bf597e8a5f
Aleksey Knigin <alexyo2626@gmail.com>
2022-09-09 23:18:03 +0300

    add task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const bodyEl = document.querySelector('body');
+
+startBtn.addEventListener('click', changeBgColor);
+
+let timerId = null;
+
+function changeBgColor() {
+  timerId = setInterval(() => {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startBtn.disabled = 'true';
+}
+
+stopBtn.addEventListener('click', stopChangeBgColor);
+
+function stopChangeBgColor() {
+  clearInterval(timerId);
+  startBtn.disabled = '';
+}
